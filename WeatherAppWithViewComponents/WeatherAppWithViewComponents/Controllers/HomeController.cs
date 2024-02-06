2d5a612651f7f7380e507b552c6b5db17e3eb752
VidaGerg98 <48093658+VidaGerg98@users.noreply.github.com>
2024-02-06 13:24:13 +0100

    Add files via upload
    init

---
+++ WeatherAppWithViewComponents/WeatherAppWithViewComponents/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppWithViewComponents.Models;
+
+namespace WeatherAppWithViewComponents.Controllers {
+	public class HomeController : Controller {
+		[Route("/")]
+		public IActionResult Index() {
+			List<CityWeather> cityWeatherList = new List<CityWeather>() {
+				new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+				new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+				new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+			};
+			ViewBag.Title = "Weather";
+			return View(cityWeatherList);
+		}
+
+		[Route("weather/{cityCode?}")]
+		public IActionResult CityWeather(string? cityCode) {
+			List<CityWeather> cityWeatherList = new List<CityWeather>() {
+				new CityWeather() { CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+				new CityWeather() { CityUniqueCode = "NYC", CityName = "New York", DateAndTime = Convert.ToDateTime("2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+				new CityWeather() { CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82}
+			};
+			CityWeather? city = cityWeatherList.Where(temp => temp.CityUniqueCode == cityCode).FirstOrDefault();
+			if (city != null) {
+				ViewBag.Title = $"{city.CityName} city weather";
+			}
+			return View(city);
+		}
+	}
+}
---
+++ WeatherAppWithViewComponents/WeatherAppWithViewComponents/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAppWithViewComponents.Models;
+
+namespace WeatherAppWithViewComponents.ViewComponents {
+    public class CityViewComponent : ViewComponent {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather city) {
+            return View(city);
+        }
+    }
+}
